0e7df742be80db9a4ee5322345b29d04b4ba9d6c
Ankit Negi <ankit40577@gmail.com>
2019-07-11 10:07:24 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const restify = require('restify');
+const config = require('./config');
+
+const server = restify.createServer();
+
+//Middleware
+server.use(restify.plugins.bodyParser());
+
+server.listen(config.PORT, () => {
+    mongoose.connect(
+        config.MONGODB_URI,
+        {useNewUrlParser: true}
+    );
+});
+
+const db = mongoose.connection;
+
+db.on('error', err => console.log(err))
+
+db.once('open', () => {
+    require('./routes/customers')(server);
+    console.log(`Server started on port ${config.PORT}`);
+});
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    URL: process.env.BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://abc!123:abc!123@ds249717.mlab.com:49717/customer_api'
+}
